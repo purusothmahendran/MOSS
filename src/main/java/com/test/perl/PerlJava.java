3f9103bcb4bcef1092d3049426cefa0a280dfae7
unknown <purusoth1091@gmail.com>
2014-04-28 21:39:22 -0500

    2.2

--- src/main/java/com/test/perl/PerlJava.java
+++ src/main/java/com/test/perl/PerlJava.java
@@ -60,10 +60,11 @@ public class PerlJava extends HttpServlet {
 			}
 			File vFiles=new File(compFiles);
 			File[] listFiles=vFiles.listFiles();
+			
 			for(int i=0;i<listFiles.length;i++)
 			{
-				response.getWriter().print(listFiles[i]);
-				fileNames.append(listFiles[i].getAbsolutePath()+" ");
+				//response.getWriter().print(listFiles[i]);
+				fileNames.append(listFiles[i]+" ");
 				//fileNames.append("");
 			}
 			
